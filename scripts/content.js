4619d05173bc46869239ea7212199e42beef5e35
Barnold88 <bandslover@outlook.com>
2025-02-06 21:23:15 +0000

    Add insert methodology so ratings can be displayed on modal cards

--- scripts/content.js
+++ scripts/content.js
@@ -3,6 +3,9 @@ var target = document.querySelector('head > title');
 var precursor = "https://www.omdbapi.com/?t="
 var API_KEY = "Please add your API key" 
 
+
+
+
 async function getData(URL) {
   
   try {
@@ -18,6 +21,15 @@ async function getData(URL) {
   }
 }
 
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+
+document.getElementById("main")
+
 
 var observer = new window.WebKitMutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
@@ -30,7 +42,10 @@ var observer = new window.WebKitMutationObserver(function(mutations) {
           getData(request)
 
           var panels = document.getElementsByClassName("detail-modal-container")
-          console.log(panels[0].children[1])
+          var subPanel = panels[0].children[1] // Get main view modal and then find subsection under the description. 
+          var insertLocation = subPanel.getElementsByClassName("ptrack-content")
+          subPanel.insertBefore(div,insertLocation[0])   
+
           
         }
         
