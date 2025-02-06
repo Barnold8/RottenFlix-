7c3f756e2d8fb5dd7bab4c1c657663a0d8ed9d29
Barnold88 <bandslover@outlook.com>
2025-02-06 20:50:12 +0000

    Add title to variable

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,16 @@
-
+var ignore = "Home - Netflix"
 var target = document.querySelector('head > title');
+
 var observer = new window.WebKitMutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
+        if(mutation.target.textContent != ignore){
+
+          title = mutation.target.textContent
+        
+
+          //Split up the name into readable text
+          
+        }
         
     });
 });
