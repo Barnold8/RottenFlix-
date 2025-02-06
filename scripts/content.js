3abda3163cbac4174ac1ce94fca810283ede6968
Barnold88 <bandslover@outlook.com>
2025-02-06 21:08:41 +0000

    Add way to get second subsection of view modal

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,23 @@
 var ignore = "Home - Netflix"
 var target = document.querySelector('head > title');
+var precursor = "https://www.omdbapi.com/?t="
+var API_KEY = "Please add your API key" 
+
+async function getData(URL) {
+  
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    // console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 
 var observer = new window.WebKitMutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
@@ -7,9 +25,12 @@ var observer = new window.WebKitMutationObserver(function(mutations) {
 
           title = mutation.target.textContent.split("-")[0] // takes the tab title, splits it into two strings given the '-' char and then takes the left side
 
-          
+          var request = precursor + title + "&apikey=" + API_KEY
 
-          
+          getData(request)
+
+          var panels = document.getElementsByClassName("detail-modal-container")
+          console.log(panels[0].children[1])
           
         }
         
